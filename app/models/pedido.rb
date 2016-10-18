7b91070a98b225cde1edf431345490cc1a91674e
AdrielLucas <adriellucas@adriellucas.com>
2016-10-17 21:32:52 -0300

    permitindo o uso da virgula

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -2,8 +2,7 @@ class Pedido < ApplicationRecord
   belongs_to :cliente
   has_many :items, dependent: :destroy
   validates_presence_of :cliente, :message => "- Deve ser preenchido"
-  validates_presence_of :items, :message => "- Deve ser preenchido"
-
+usar_como_dinheiro :desconto
   accepts_nested_attributes_for :items, :allow_destroy => true
   def totalPedidos
       items.sum {|item| item.produto.valor * item.quantidade }
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,6 +1,7 @@
 class Produto < ApplicationRecord
   validates_presence_of :nome, :message => "- Deve ser preenchido"
   validates_uniqueness_of :nome, :messa => "- Produto já foi cadastrado"
+  usar_como_dinheiro :valor
   has_many :pedidos
   has_many :items, through: :pedidos
 end
